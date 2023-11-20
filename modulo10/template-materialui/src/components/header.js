477af6e491741d55408ee5bcf4a3479cccab5ae0
Gabriel Pires <p.gabriel.pires@viveo.com.br>
2023-11-20 20:05:17 -0300

    Styles Modificados

---
+++ modulo10/template-materialui/src/components/header.js
@@ -0,0 +1,35 @@
+import React from 'react';
+import AppBar from '@mui/material/AppBar';
+import Box from '@mui/material/Box';
+import Toolbar from '@mui/material/Toolbar';
+import Typography from '@mui/material/Typography';
+import IconButton from '@mui/material/IconButton';
+import MenuIcon from '@mui/icons-material/Menu';
+import Button from '@mui/material/Button'; // Import Button from @mui/material
+
+function Header() {
+  return (
+    <Box sx={{ flexGrow: 1 }}>
+      <AppBar position="static">
+        <Toolbar>
+          <IconButton
+            size="large"
+            edge="start"
+            color="inherit"
+            aria-label="menu"
+            sx={{ mr: 2 }}
+          >
+            <MenuIcon />
+          </IconButton>
+          <Typography variant="h6" component="div" sx={{ flexGrow: 1 }}>
+            Gerenciamento de Projetos
+          </Typography>
+          {/* Add additional buttons or components on the right if needed */}
+          <Button color="inherit">Sair</Button>
+        </Toolbar>
+      </AppBar>
+    </Box>
+  );
+}
+
+export default Header;
